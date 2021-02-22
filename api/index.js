7bbf52906b09cb47b00320cd809fffb63c26f731
Yavila <kixtersama@gmail.com>
2021-02-21 19:05:49 -0600

    Uso de Dependencias

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-    res.send('Hola Mundo');
-}
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyparser.json())
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifiedTopology: true })
+
+app.get('*', (req, res) => {
+    res.send('chanchito feliz')
+})
+module.exports = app
