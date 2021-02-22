c7cbbdc857f04aeb6f6c40e2f0c6b0201d408429
Yavila <kixtersama@gmail.com>
2021-02-21 20:28:54 -0600

    Estructura de BD

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyparser.json())
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get('*', (req, res) => {
     res.send('chanchito feliz')
